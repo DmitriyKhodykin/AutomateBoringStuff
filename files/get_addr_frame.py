da6b337e72b55fad90b2206f34e528e92a12fee8
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2021-01-20 22:22:06 +0300

    Create get_addr_frame.py

---
+++ files/get_addr_frame.py
@@ -0,0 +1,18 @@
+# Импорты
+import os
+import pandas as pd
+
+
+def get_hierarchy_frame(data_dir: str) -> pd.DataFrame:
+    """Возвращает датасет с путями до файлов
+    в папке и ее подпапках. Папки помечаются
+    как лейблы графических файлов.
+    """
+    
+    folders = os.listdir(data_dir)
+    files = []
+    for i in folders:
+      for j in os.listdir(f'{data_dir}/{i}'):
+        files.append([i, data_dir+'/'+i+'/'+j])
+    return pd.DataFrame(files, columns=['lbl', 'addr'],
+                        index=None)
