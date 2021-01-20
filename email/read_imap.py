3ff523f303b4f377b902fddb1364a2b7595d9358
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2021-01-20 16:47:52 +0300

    Update read_imap.py

--- email/read_imap.py
+++ email/read_imap.py
@@ -1,40 +1,36 @@
 # Импорты
-import pandas as pd
-from auth import auth
 import imaplib
 import base64
-import time
 import re
 
 
-def read_mail():
+def read_mail(folder_name: str, server_name: str,
+             usr_name: str, passwd: str) -> list:
     """Выгрузка тела каждого письма из почтовой папки.
     Предварительно, на почтовом сервере должна быть создана
-    папка Vialon, в которую приходят системные уведомления.
-    Формат уведомлений:
-        Volvo о565ам31 вошел в ООО КДВ МЖД. 27 окт 2020 13:12:45 он двигался 
-        со скоростью 27 км/ч около 'Володарского ул., Армавир, Краснодарский край, Россия'
+    папка folder_name, в которую приходят сообщения.
     """
     
     decode_list = []
-    print("Connecting to {}...".format(server))
+    print(f"Подключение к серверу {server_name}...")
     imap = imaplib.IMAP4_SSL(server)
-    print("Connected! Logging in as {}...".format(login))
-    imap.login(login, password)
-    print("Logged in! Listing messages...")
+    print(f"Подключено! Пользователь {usr_name}...")
+    imap.login(usr_name, passwd)
+    print("Чтение сообщений...")
 
-    status, select_data = imap.select('Vialon')
+    status, select_data = imap.select(folder_name)
     select_data[0].decode('utf-8')  # Количество писем в ящике
     status, search_data = imap.search(None, 'ALL')
 
     for msg_id in search_data[0].split():
         status, msg_data = imap.fetch(msg_id, '(RFC822)')
         msg_text = (msg_data[0][-1]).decode('utf-8')
-        encode_body = msg_text.split('base64')[1][4:-45]
+        encode_body = msg_text.split('base64')
         decode_body = base64.b64decode(encode_body).decode('utf-8')
         decode_body = re.sub(r"'", "", decode_body)
         decode_list.append(decode_body)
         imap.store(msg_id, '+FLAGS', '\\Deleted')
     imap.expunge()
     imap.logout()
+    print("Выполнено. Выход из учетной записи...")
     return decode_list
