88563a329feddc331946c093bbbd71bc25579fd3
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2021-01-20 22:26:20 +0300

    Update get_addr_frame.py

--- files/get_addr_frame.py
+++ files/get_addr_frame.py
@@ -12,7 +12,19 @@ def get_hierarchy_frame(data_dir: str) -> pd.DataFrame:
     folders = os.listdir(data_dir)
     files = []
     for i in folders:
-      for j in os.listdir(f'{data_dir}/{i}'):
-        files.append([i, data_dir+'/'+i+'/'+j])
+        for j in os.listdir(f'{data_dir}/{i}'):
+            files.append([i, data_dir+'/'+i+'/'+j])
     return pd.DataFrame(files, columns=['lbl', 'addr'],
                         index=None)
+
+
+def get_flat_frame(data_dir: str) -> pd.DataFrame:
+      """Возвращает датасет с путями до файлов
+      в папке (неразмеченные данные).
+      """
+        
+      files = []
+      for i in os.listdir(data_dir):
+          files.append([data_dir+'/'+i])
+      return pd.DataFrame(files, columns=['addr'],
+                          index=None)
