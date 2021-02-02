ec4fd144bba743e7a29e45bd894d07179fa7e038
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2021-02-02 14:00:36 +0300

    Update psa_pic.py

--- img/psa_pic.py
+++ img/psa_pic.py
@@ -1,4 +1,5 @@
-import numpy as np
+import numpy as np      # Массивы и вычисления
+import cv2              # OpenCV
 
 
 def psa_img(img):
@@ -13,3 +14,16 @@ def psa_img(img):
     vec = np.dot(v, img)
     return vec
     
+
+def psa_pic(addr: str) -> np.array:
+    """
+    Проецирует матрицу изображения
+    на одну ось пространства (вектор).
+    addr - локальный путь к файлу.
+    """
+    img = cv2.imread(addr, cv2.IMREAD_GRAYSCALE)
+    covmat = np.cov(img)
+    _, vecs = np.linalg.eig(covmat)
+    v = -vecs[:, 1]
+    vec = np.dot(v, img)
+    return vec
